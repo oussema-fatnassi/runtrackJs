48c2aacd1da7d090b4c5642b2299441042a5b2db
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-04-15 11:49:42 +0200

    Job05 finished

---
+++ jour01/job05/script.js
@@ -0,0 +1,7 @@
+function showWeekDays() {
+    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    for  (let i = 0; i < days.length; i++) {
+        console.log(days[i]);
+    }
+}
+showWeekDays();
