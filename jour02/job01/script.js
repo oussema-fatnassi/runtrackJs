7793319325f8bda5ccd55d212eaf52c34eebdae3
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-04-16 11:25:38 +0200

    Creation of jour02 dossier and job01 finished

---
+++ jour02/job01/script.js
@@ -0,0 +1,4 @@
+function quote() {
+    console.log(document.getElementById("quote").textContent);              // Show the quote in the console           
+    alert(document.getElementById("quote").textContent);                    // Show the quote in an alert
+}
