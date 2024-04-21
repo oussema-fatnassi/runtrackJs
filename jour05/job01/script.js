0bf2a3591526fb0c1c95e2db381ccea51f7c7625
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-04-21 12:02:00 +0200

    Async function checkInputs, testing save inputs in JSON

--- jour05/job01/script.js
+++ jour05/job01/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    const form = $('#form');
     const firstName = $('#first_name');
     const lastName = $('#last_name');
     const email = $('#email');
@@ -10,6 +11,15 @@ $(document).ready(function(){
     $('#register').on('click', function(e) {
         e.preventDefault();
         checkInputs();
+        var test = JSON.stringify({
+            first_name: firstName.val(),
+            last_name: lastName.val(),
+            email: email.val(),
+            password: password.val(),
+            address: address.val(),
+            postal_code: postalCode.val()
+        });
+        console.log(test);
     });
     
     const setErrorFor = (input, message) => {
@@ -38,7 +48,7 @@ $(document).ready(function(){
         return re.test(String(email).toLowerCase());
     }
 
-    function checkInputs() {
+    async function checkInputs() {
         const firstNameValue = firstName.val().trim();
         const lastNameValue = lastName.val().trim();
         const emailValue = email.val().trim();
@@ -47,80 +57,106 @@ $(document).ready(function(){
         const addressValue = address.val().trim();
         const postalCodeValue = postalCode.val().trim();
 
-        // First Name
-        if(firstNameValue === '') {
-            setErrorFor(firstName, 'First Name cannot be blank');
-        } 
-        else if (firstNameValue.length < 3) {
-            setErrorFor(firstName, 'First Name must be at least 3 characters');
-        }
-        else {
-            setSuccessFor(firstName);
-        }
-        // Last Name
-        if(lastNameValue === '') {
-            setErrorFor(lastName, 'Last Name cannot be blank');
-        } 
-        else if (lastNameValue.length < 3) {
-            setErrorFor(lastName, 'Last Name must be at least 3 characters');
-        }
-        else {
-            setSuccessFor(lastName);
-        }
-        // Email
-        if(emailValue === '') {
-            setErrorFor(email, 'Email cannot be blank');
-        } else if (!isEmail(emailValue)) {
-            setErrorFor(email, 'Email is not valid');
-        } else {
-            setSuccessFor(email);
-        }
-        // Password
-        if (passwordValue === '') {
-            setErrorFor(password, 'Password cannot be blank');
+        const validatePromise = new Promise((resolve, reject) => {
+            // First Name
+            if(firstNameValue === '') {
+                setErrorFor(firstName, 'First Name cannot be blank');
+                reject();
+            } 
+            else if (firstNameValue.length < 3) {
+                setErrorFor(firstName, 'First Name must be at least 3 characters');
+                reject();
+            }
+            else {
+                setSuccessFor(firstName);
+                resolve();
+            }
+            // Last Name
+            if(lastNameValue === '') {
+                setErrorFor(lastName, 'Last Name cannot be blank');
+                reject();
+            } 
+            else if (lastNameValue.length < 3) {
+                setErrorFor(lastName, 'Last Name must be at least 3 characters');
+                reject();
+            }
+            else {
+                setSuccessFor(lastName);
+                resolve();
+            }
+            // Email
+            if(emailValue === '') {
+                setErrorFor(email, 'Email cannot be blank');
+                reject();
+            } else if (!isEmail(emailValue)) {
+                setErrorFor(email, 'Email is not valid');
+                reject();
+            } else {
+                setSuccessFor(email);
+                resolve();
+            }
+            // Password
+            if (passwordValue === '') {
+                setErrorFor(password, 'Password cannot be blank');
+                reject();
+            }
+            else if (passwordValue.length < 8) {
+                setErrorFor(password, 'Password must be at least 8 characters');
+                reject();
+            }
+            else if (!/[A-Z]/.test(passwordValue)) {
+                setErrorFor(password, 'Password must contain at least one uppercase letter');
+                reject();
+            }
+            else if (!/[a-z]/.test(passwordValue)) {
+                setErrorFor(password, 'Password must contain at least one lowercase letter');
+                reject();
+            }
+            else if (!/\d/.test(passwordValue)) {
+                setErrorFor(password, 'Password must contain at least one number');
+                reject();
+            }
+            else if (!/[!\"#$%&'()*+,-.\/:;<=>?@[\\\]^_`{|}~]/.test(passwordValue)) {
+                setErrorFor(password, 'Password must contain at least one special character');
+                reject();
+            }
+            else {
+                setSuccessFor(password);
+                resolve();
+            }
+            // Password Confirm
+            if(passwordConfirmValue === '') {
+                setErrorFor(passwordConfirm, 'Password cannot be blank');
+                reject();
+            } else if(passwordValue !== passwordConfirmValue) {
+                setErrorFor(passwordConfirm, 'Passwords do not match');
+                reject();
+            } else{
+                setSuccessFor(passwordConfirm);
+                resolve();
+            }
+            // Address
+            if(addressValue === '') {
+                setErrorFor(address, 'Address cannot be blank');
+                reject();
+            } else {
+                setSuccessFor(address);
+                resolve();
+            }
+            // Postal Code
+            if(postalCodeValue === '') {
+                setErrorFor(postalCode, 'Postal Code cannot be blank');
+                reject();
+            } else {
+                setSuccessFor(postalCode);
+                resolve();
+            }
+        });
+
+        try {
+            await validatePromise;
+        } catch (error) {
             return;
-        }
-        else if (passwordValue.length < 8) {
-            setErrorFor(password, 'Password must be at least 8 characters');
-            return;
-        }
-        else if (!/[A-Z]/.test(passwordValue)) {
-            setErrorFor(password, 'Password must contain at least one uppercase letter');
-            return;
-        }
-        else if (!/[a-z]/.test(passwordValue)) {
-            setErrorFor(password, 'Password must contain at least one lowercase letter');
-            return;
-        }
-        else if (!/\d/.test(passwordValue)) {
-            setErrorFor(password, 'Password must contain at least one number');
-            return;
-        }
-        else if (!/[!\"#$%&'()*+,-.\/:;<=>?@[\\\]^_`{|}~]/.test(passwordValue)) {
-            setErrorFor(password, 'Password must contain at least one special character');
-            return;
-        }
-        
-        setSuccessFor(password);
-        // Password Confirm
-        if(passwordConfirmValue === '') {
-            setErrorFor(passwordConfirm, 'Password cannot be blank');
-        } else if(passwordValue !== passwordConfirmValue) {
-            setErrorFor(passwordConfirm, 'Passwords do not match');
-        } else{
-            setSuccessFor(passwordConfirm);
-        }
-        // Address
-        if(addressValue === '') {
-            setErrorFor(address, 'Address cannot be blank');
-        } else {
-            setSuccessFor(address);
-        }
-        // Postal Code
-        if(postalCodeValue === '') {
-            setErrorFor(postalCode, 'Postal Code cannot be blank');
-        } else {
-            setSuccessFor(postalCode);
         }
     }
 });
